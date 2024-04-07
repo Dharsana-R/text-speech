3f6a5cfe2675cf96f29f1c524e568c80463027c8
Dharsana <126599832+Dharsana-R@users.noreply.github.com>
2024-04-07 21:03:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
 import streamlit as st
 import pyttsx3
 
+# Custom CSS style
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# Function to convert text to speech
 def text_to_speech(text, rate):
     engine = pyttsx3.init()
     engine.setProperty('rate', rate)
     engine.say(text)
     engine.runAndWait()
 
+# Main function
 def main():
     st.title("Text-to-Speech")
-    
-    st.write("Enter the text ")
-    text_input = st.text_area("Input Text")
 
+    # Load custom CSS
+    local_css("styles.css")
+
+    # Input text area
+    st.write("")
+    text_input = st.text_area("Enter Input Text")
+
+    # Speech rate slider
     rate = st.slider("Select Speech Rate", min_value=100, max_value=300, step=10, value=200)
 
+    # Convert to speech button
     if st.button("Convert to Speech"):
         if text_input:
             text_to_speech(text_input, rate)
